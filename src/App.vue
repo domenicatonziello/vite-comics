98e4eaa653e42dcb05d4378ea21439bec44ca9c0
domenicatonziello <domenica.tonziello10@gmail.com>
2023-01-20 15:46:03 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: 'Vite Comics'
+}
+</script>
+<template>
+  <!-- Header -->
+  <header></header>
+  <!-- Main -->
+  <main></main>
+  <!-- Footer -->
+  <footer></footer>
+</template>
+
+
+<style>
+
+</style>
